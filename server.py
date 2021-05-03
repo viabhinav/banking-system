a9d9e41a05b5eb2e5fb19813ae6241e87c1ca9cd
viabhinav <viabhinav33@gmail.com>
2021-05-03 12:29:29 +0530

    Major update with limitations and advanced errors

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import pickledb as dbms
 from werkzeug.exceptions import abort
 from random import randint
+import datetime
 
 db = dbms.load("customers.json", True)
 
@@ -19,6 +20,10 @@ bal = list()
 for customer in customers:
     bal.append(db.get(customer))
     cust[customer] = db.get(customer)
+    
+def error(error, desc):
+    resp = make_response(render_template("error.html", error=error, desc=desc))
+    return resp
 
 print(list(zip(cust,bal)))
 @app.route('/')
@@ -32,9 +37,14 @@ def view_customers():
 
 @app.route('/transaction-page', methods = ['GET'])
 def transactionpage():
-    return render_template("transactionpage.html", customers=customers, i=1, h="s")
-
-
+    if (request.cookies.get("user")):
+        if (request.args.get('to')):
+            return render_template("transactionpage.html", customers=customers, i=1,user=request.cookies.get("user"), h=request.args.get("to"))
+        else:
+            return render_template("transactionpage.html", customers=customers, i=1, user=request.cookies.get("user"))
+    else:
+        return error("You must login to do transactions.", "Login <a href = '/loginpage'>here</a>")
+        
 @app.route('/processtran',methods = ['GET'])
 def processtrans():
     fromx = ''
@@ -70,6 +80,29 @@ def processtrans():
 def custinfo(customerx):
     customern = customerx.replace('+', ' ')
     return render_template("customerinfo.html", name=customern, bal=db.get(customern))
+
+@app.route('/login')
+def login():
+    resp = make_response(render_template("login.html"))
+    return resp
+
+@app.route('/processlogin', methods = ['GET'])
+def processlogin():
+    if request.args.get('username') and (request.args.get('username') in customers):
+        expire_date = datetime.datetime.now()
+        expire_date = expire_date + datetime.timedelta(days=90)
+        resp = make_response(render_template("successfullogin.html"))
+        resp.set_cookie("user", request.args.get('username'), expires=expire_date)
+        return resp
+    else:
+        abort(401)
+        
+@app.route('/statuscode', methods = ['GET'])
+def status():
+    if(request.args.get('code')):
+        abort(int(request.args.get('code')))
+    else:
+        return 'Enter a code'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8000)
